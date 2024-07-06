b20245e12bf5c088b5de44896547e684f052b62d
kath424 <acostak424@gmail.com>
2024-07-06 11:28:18 +0000

    implementa api

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from operator import and_
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,7 +9,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import Favorite, Planet, db, User, People
+import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +38,152 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    return jsonify([p.toDict() for p in people]), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people', methods=['POST'])
+def post_people():
+    new_person = People()
+    
+    new_person.name = request.json['name']
+    new_person.gender = request.json['gender']
+    new_person.hair_color = request.json['hair_color']
+    new_person.height = request.json['height']
 
-    return jsonify(response_body), 200
+    db.session.add(new_person)
+    db.session.commit()
+    
+    return jsonify({
+        "message": 'person created',
+        "user_id": new_person.id
+    }),201
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get(people_id)
+    return jsonify(person.toDict()),200
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([p.toDict() for p in planets]), 200
+
+@app.route('/planets', methods=['POST'])
+def post_planets():
+    new_planet = Planet()
+
+    new_planet.name = request.json['name']
+    new_planet.diameter = request.json['diameter']
+    new_planet.climate = request.json['climate']
+    new_planet.population = request.json['population']
+
+    db.session.add(new_planet)
+    db.session.commit()
+    
+    return jsonify({
+        "message": 'planet created',
+        "user_id": new_planet.id
+    }),201
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    return jsonify(planet.toDict()),200
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([u.toDict() for u in users]), 200
+
+@app.route('/users', methods=['POST'])
+def post_user():
+    new_user = User()
+
+    new_user.email = request.json['email']
+    new_user.name = request.json['name']
+    new_user.lastname = request.json['lastname']
+    new_user.password = request.json['password']
+    new_user.subscription_date = datetime.datetime.now()
+
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({
+        "message": 'usuario creado',
+        "user_id": new_user.id
+    }),201
+
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    user_id = request.args.get('user_id')
+
+    user_favorites = Favorite.query.filter_by(user_id=user_id)
+    favorites = [uf.toDict() for uf in user_favorites]
+    favorites_mapped = [uf['people'] if uf['planet'] is None  else uf['planet'] for uf in favorites]
+    return jsonify(favorites_mapped)
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def post_favorite_planet(planet_id):
+    user_id = request.args.get('user_id')
+
+    favorite = Favorite()
+
+    favorite.planet_id = planet_id
+    favorite.user_id = user_id
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify({
+        "message": 'favorite planet created',
+        "favorite_id": favorite.id
+    }),201
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def post_favorite_people(people_id):
+    user_id = request.args.get('user_id')
+
+    favorite = Favorite()
+    
+    favorite.people_id = people_id
+    favorite.user_id = user_id
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify({
+        "message": 'favorite people created',
+        "favorite_id": favorite.id
+    }),201
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_favorite(planet_id):
+    user_id = int(request.args.get('user_id'))
+    
+    user_favorite = Favorite.query.filter_by(planet_id=planet_id, user_id=user_id).one()
+
+    db.session.delete(user_favorite)
+    db.session.commit()
+
+    return jsonify({ "msg": "borrado"}), 202
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_people_favorite(people_id):
+    user_id = request.args.get('user_id')
+    
+    user_favorite = Favorite.query.filter_by(people_id=people_id, user_id=user_id).one()
+
+    db.session.delete(user_favorite)
+    db.session.commit()
+    return jsonify({ "msg": "borrado"}), 202
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,58 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    lastname = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    subscription_date = db.Column(db.Date(), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "Objeto User "+ self.name
+    
+    def toDict(self):
+       return { "id":self.id, "name":self.name,  "lastname":self.lastname, "email":self.email }
+    
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer, nullable=False )
+    hair_color = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    def toDict(self):
+       return { "id":self.id, "name":self.name,  "height":self.height, "hair_color":self.hair_color, "gender":self.gender }
+
+class Planet(db.Model):
+    __tablename__= 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    climate =  db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+
+    def toDict(self):
+       return { "id":self.id, "name":self.name,  "diameter":self.diameter, "climate":self.climate, "population":self.population }
+
+class Favorite(db.Model):
+    __tablename__= 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    people = db.relationship(People)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    planet = db.relationship(Planet)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship(User)
+
+    def toDict(self):
+       return { "id":self.id, "people_id":self.people_id,  
+               "people": None if self.people is None else  self.people.toDict(), 
+               "planet_id":self.planet_id, 
+               "planet": None if self.planet is None else  self.planet.toDict(),
+               "user_id": self.user_id
+                }
+
+
